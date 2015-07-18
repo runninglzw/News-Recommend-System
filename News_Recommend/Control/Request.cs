68149e81d45d26d29eaa5836d463152c6fb4e84a
runninglzw <459437440@qq.com>
2015-07-18 21:58:21 +0800

    添加网络请求类和Json解析类
    网络请求类负责对API进行请求并得到Json字符串
    Json解析类负责将Json字符串反序列化得到一个新闻集合

---
+++ News_Recommend/Control/Request.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+using News_Recommend.Model;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace News_Recommend.Control
+{
+    public class Request
+    {
+        /// <summary>
+        /// 对新闻接口进行请求返回json数据（字符串）
+        /// </summary>
+        /// <param name="url"></param>接口的url
+        /// size 为获取新闻的个数
+        /// <returns></returns>
+        public static string createurl(string url,int size)
+        {
+            string myurl = url + "?size=" + size;
+            WebRequest request = WebRequest.Create(myurl);
+            IAsyncResult result = request.BeginGetResponse(null, request);
+            WebResponse response = request.EndGetResponse(result);
+            Stream stream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(stream);
+            string res = reader.ReadToEnd();
+            return res;
+        }
+        /// <summary>
+        /// 将返回的json字符串进行解析，将解析得到的新闻数据填充到一个新闻的集合中
+        /// </summary>
+        /// <param name="data"></param>   json字符串
+        /// <returns></returns>
+        public static List<News> analysis(string data)
+        {
+            List<News> newslist=new List<News>();
+            
+            JObject jo = (JObject)JsonConvert.DeserializeObject(data);//将字符串转化为JObject
+            JArray ja = (JArray)jo["detail"];//索引到detail
+            foreach (JToken jt in ja)
+            {
+                string id = jt["group_id"].ToString();//获得id
+                string title = jt["title"].ToString();//获得标题
+                string source = jt["source"].ToString();//获得新闻来源
+                string url = jt["article_url"].ToString();//获得新闻url
+                DateTime include_time = new DateTime(Convert.ToInt64(jt["behot_time"].ToString()));//获得新闻收录时间,将获得的毫秒数转化为datetime
+                int praise_count = Convert.ToInt32(jt["digg_count"].ToString());//获得新闻被赞次数
+                int step_count = Convert.ToInt32(jt["bury_count"].ToString()); //新闻踩的次数
+                int collect_count = Convert.ToInt32(jt["repin_count"].ToString());//新闻收藏的次数
+                News news = new News(id,title,source,url,include_time,praise_count,step_count,collect_count);
+                newslist.Add(news);
+            }
+            return newslist;
+        }
+    }
+}
---
+++ News_Recommend/Model/News.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace News_Recommend.Model
+{
+    public class News
+    {
+        string id;
+        string source;//新闻来源
+        string title;//新闻的标题
+        string url;//新闻的url
+        DateTime include_time;//新闻收录时间
+        int praise_count;//新闻赞的次数
+        int step_count;//新闻踩的次数
+        int collect_count;//新闻收藏的次数
+        public News()
+        {
+ 
+        }
+        public News(string id,
+        string title,
+        string source,
+        string url,
+        DateTime include_time,
+        int praise_count,
+        int step_count,
+        int collect_count)
+        {
+            this.id = id;
+            this.title = title;
+            this.source = source;
+            this.url = url;
+            this.include_time = include_time;
+            this.praise_count = praise_count;
+            this.step_count = step_count;
+            this.collect_count = collect_count;
+        }
+        public string Getid()
+        {
+            return id;
+        }
+        public string Gettitle()
+        {
+            return title;
+        }
+        public string Geturl()
+        {
+            return url;
+        }
+        public DateTime Getinclude_time()
+        {
+            return include_time;
+        }
+        public int Getpraise_count()
+        {
+            return praise_count;
+        }
+        public int Getstep_count()
+        {
+            return step_count;
+        }
+        public int Getcollect_count()
+        {
+            return collect_count;
+        }
+    }
+}
